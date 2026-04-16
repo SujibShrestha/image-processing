9062795bdc40ed8bf0177d8f28b1c9bd35a861bd
SujibShrestha <sujeeb98@gmail.com>
2026-04-16 16:55:51 +0545

    Implemenent Logger, Cors , morgan and Helment for logginfg and enchancing Security respectively

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,12 +1,45 @@
-import express from 'express'
+import express, { NextFunction, Request, Response } from "express";
+import morgan from "morgan";
+import logger from "./utils/logger.js";
+import helmet from 'helmet'
+import 'dotenv/config'
+import cors from "cors"
 
-const app = express()
-const PORT = process.env.PORT || 3000
+const app = express();
+const PORT = process.env.PORT || 3000;
 
-app.get("/",(req,res)=>{
-    res.status(200).json("Api is running")
-})
+// middlewares
+app.use(helmet())
+app.use(cors())
+app.use(
+  morgan("combined", {
+    stream: {
+      write: (message) => logger.info(message.trim()),
+    },
+  }),
+);
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
-app.listen(PORT,()=>{
-    console.log(`Server is running on PORT: ${PORT}`)
-})
+
+// Global Error Handler
+app.use((err:Error, req:Request, res:Response, next:NextFunction) => {
+  logger.error({
+    message: err.message,
+    stack: err.stack,
+  });
+
+  res.status(500).json({ error: "Internal Server Error" });
+});
+
+// Health Check
+app.get("/", (req, res) => {
+  res.status(200).json({
+    status: "success",
+    message: "API is running",
+  });
+});
+
+app.listen(PORT, () => {
+  logger.info(`Server is running on PORT: ${PORT}`);
+});
